184cae117cecb626f361f4cc057bc44b6c948e78
aajovan <alenjovancom@gmail.com>
2020-09-02 15:13:12 +0200

    Updates

---
+++ precache-manifest.80f1b9ddd7168dca324947044d3022ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b41d235e1bf8b37de6f7afff77043ae",
+    "url": "/reactvrijeme/index.html"
+  },
+  {
+    "revision": "74943bcfeb1d91485d76",
+    "url": "/reactvrijeme/static/css/2.c3674376.chunk.css"
+  },
+  {
+    "revision": "6e7e6657922ea46d6a68",
+    "url": "/reactvrijeme/static/css/main.8e82b613.chunk.css"
+  },
+  {
+    "revision": "74943bcfeb1d91485d76",
+    "url": "/reactvrijeme/static/js/2.00a81fdf.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/reactvrijeme/static/js/2.00a81fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7e6657922ea46d6a68",
+    "url": "/reactvrijeme/static/js/main.e1a5e07a.chunk.js"
+  },
+  {
+    "revision": "a1288298e320b04d3837",
+    "url": "/reactvrijeme/static/js/runtime-main.cf86d46c.js"
+  },
+  {
+    "revision": "2411d418743f92e0b5c5ab76e632648e",
+    "url": "/reactvrijeme/static/media/logo.2411d418.png"
+  }
+]);
